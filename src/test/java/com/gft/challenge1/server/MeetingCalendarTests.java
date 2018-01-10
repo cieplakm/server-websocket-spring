2d16070ae2fbd42ad6387433ebe35a2c774706c9
Mateusz Cieplak <mateusz.cieplak@gft.com>
2018-01-10 11:41:03 +0100

    Change test way

--- src/test/java/com/gft/challenge1/server/MeetingCalendarTests.java
+++ src/test/java/com/gft/challenge1/server/MeetingCalendarTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.*;
@@ -16,14 +17,24 @@ public class MeetingCalendarTests {
     @Test
     public void iterateThroughMeetingDaysTest(){
         MeetingCalendar meetingCalendar = new MeetingCalendar(2016,9,19);
+        DayOfWeek[] meetingsDayOfWeek = {DayOfWeek.FRIDAY, DayOfWeek.TUESDAY};
 
-        assertThat(meetingCalendar).extractingResultOf("toString").contains(LocalDate.of(2016,9,20).toString());
-        assertThat(meetingCalendar).extractingResultOf("toString").contains(LocalDate.of(2016,9,23).toString());
-        assertThat(meetingCalendar).extractingResultOf("toString").contains(LocalDate.of(2016,9,27).toString());
-        assertThat(meetingCalendar).extractingResultOf("toString").contains(LocalDate.of(2016,9,30).toString());
-        assertThat(meetingCalendar).extractingResultOf("toString").doesNotContain(LocalDate.of(2016,9,21).toString());
-        assertThat(meetingCalendar).extractingResultOf("toString").doesNotContain(LocalDate.of(2016,9,22).toString());
-        assertThat(meetingCalendar).extractingResultOf("toString").doesNotContain(LocalDate.of(2016,9,24).toString());
+        assertThat(meetingCalendar)
+                .extractingResultOf("getDayOfWeek")
+                .contains(meetingsDayOfWeek);
+
+        DayOfWeek[] meetingsDayOfWeek2 = {DayOfWeek.MONDAY, DayOfWeek.WEDNESDAY};
+
+        meetingCalendar.setMeetingDays(meetingsDayOfWeek2);
+
+        assertThat(meetingCalendar)
+                .extractingResultOf("getDayOfWeek")
+                .contains(meetingsDayOfWeek2);
+
+        assertThat(meetingCalendar)
+                .extractingResultOf("getDayOfWeek")
+                .doesNotContain(meetingsDayOfWeek);
+
 
     }
 }
