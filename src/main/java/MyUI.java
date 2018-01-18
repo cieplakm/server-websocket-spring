bf71afc4239527c064e279e5008be9d2d6675859
Mateusz Cieplak <mateusz.cieplak@gft.com>
2018-01-18 14:57:21 +0100

    Remove some dependencies form pom

---
+++ src/main/java/MyUI.java
@@ -0,0 +1,22 @@
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@SpringUI
+public class MyUI extends UI{
+    VerticalLayout verticalLayout;
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        verticalLayout = new VerticalLayout();
+
+        setContent(verticalLayout);
+    }
+
+    public void append(String string){
+        verticalLayout.addComponent(new Label(string));
+    }
+
+}
