6482bd2ddd58c9ad389d49cddce952607bc58c0c
Mateusz Cieplak <mateusz.cieplak@gft.com>
2018-01-19 09:19:38 +0100

    Refactor NodeService

--- src/main/java/com/gft/challenge1/server/config/MainConfig.java
+++ src/main/java/com/gft/challenge1/server/config/MainConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 public class MainConfig {
     @Bean
-    NodeFakeRepository nodeRepozitory(){
+    NodeFakeRepository fakeRepository(){
         return new NodeFakeRepository();
     }
 }
--- src/main/java/com/gft/challenge1/server/services/NodeService.java
+++ src/main/java/com/gft/challenge1/server/services/NodeService.java
@@ -1,30 +1,28 @@
 package com.gft.challenge1.server.services;
 
 import com.gft.challenge1.server.node.NodeFakeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NodeService {
+    private NodeFakeRepository fakeRepository;
 
-    @Autowired
-    NewsService newsService;
-    @Autowired
-    NodeFakeRepository nodeRepozitory;
+    public NodeService(NodeFakeRepository fakeRepository) {
+        this.fakeRepository = fakeRepository;
+    }
 
-    @RequestMapping(path = "/addnode")
+    @RequestMapping(path = "/add")
     public String addNode(@RequestParam(value = "name", defaultValue = "New Node") String name){
-        nodeRepozitory.addNewNode(name);
-        newsService.sendWholeDataToAllObservers();
+        fakeRepository.addNewNode(name);
         return "Node \"" + name + "\"created successfully.";
     }
 
     @RequestMapping(path = "/delete")
     public String delNode(@RequestParam(value = "name") String name){
-        nodeRepozitory.removeNode(name);
-        newsService.sendWholeDataToAllObservers();
+        fakeRepository.removeNode(name);
         return "Node \"" + name + "\"deleted successfully.";
     }
+
 }
