bbaab01e6ba6831eab15eade6b258a4c1570ec37
Mateusz Cieplak <mateusz.cieplak@gft.com>
2018-01-22 07:52:17 +0100

    Refactor WebsocketConfig

--- src/main/java/com/gft/challenge1/server/config/WebSocketConfig.java
+++ src/main/java/com/gft/challenge1/server/config/WebSocketConfig.java
@@ -2,32 +2,25 @@ package com.gft.challenge1.server.config;
 
 import com.gft.challenge1.server.websockets.MyWebSocketHandler;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
+    private MyWebSocketHandler myWebSocketHandler;
+
     @Autowired
-    MyWebSocketHandler customHandler;
+    public WebSocketConfig(MyWebSocketHandler myWebSocketHandler) {
+        this.myWebSocketHandler = myWebSocketHandler;
+    }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
-        webSocketHandlerRegistry.addHandler(customHandler, "/name_of_handler");
+        webSocketHandlerRegistry.addHandler(myWebSocketHandler, "/name_of_handler");
     }
-
-    @Bean
-    public ServletServerContainerFactoryBean createWebSocketCointiner(){
-        ServletServerContainerFactoryBean containerFactoryBean = new ServletServerContainerFactoryBean();
-        containerFactoryBean.setMaxBinaryMessageBufferSize(8192);
-        containerFactoryBean.setMaxBinaryMessageBufferSize(8192);
-
-        return containerFactoryBean;
-    }
-
+    
 }
